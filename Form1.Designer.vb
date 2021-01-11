b496b46e9c1c0ec98d52a160fc99a8f4752e8114
Libor Knappek <libor.knappek@gmail.com>
2021-01-11 16:51:59 +0100

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvTable1 = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbInput = New System.Windows.Forms.TextBox()
+        Me.tbOutput = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Enabled = False
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(270, 276)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.Location = New System.Drawing.Point(471, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 29)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Tabulka"
+        '
+        'dgvTable1
+        '
+        Me.dgvTable1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvTable1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvTable1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.dgvTable1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvTable1.ColumnHeadersVisible = False
+        Me.dgvTable1.Location = New System.Drawing.Point(577, 13)
+        Me.dgvTable1.Name = "dgvTable1"
+        Me.dgvTable1.RowHeadersVisible = False
+        Me.dgvTable1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        Me.dgvTable1.RowTemplate.Height = 30
+        Me.dgvTable1.RowTemplate.ReadOnly = True
+        Me.dgvTable1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvTable1.Size = New System.Drawing.Size(312, 256)
+        Me.dgvTable1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label2.Location = New System.Drawing.Point(281, 307)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(290, 29)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Zadej počet čísel k součtu"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label3.Location = New System.Drawing.Point(375, 515)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(143, 29)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Výsledek je:"
+        '
+        'tbInput
+        '
+        Me.tbInput.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbInput.Location = New System.Drawing.Point(577, 311)
+        Me.tbInput.Name = "tbInput"
+        Me.tbInput.Size = New System.Drawing.Size(100, 26)
+        Me.tbInput.TabIndex = 4
+        '
+        'tbOutput
+        '
+        Me.tbOutput.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbOutput.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.tbOutput.Location = New System.Drawing.Point(577, 515)
+        Me.tbOutput.Multiline = True
+        Me.tbOutput.Name = "tbOutput"
+        Me.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbOutput.Size = New System.Drawing.Size(410, 80)
+        Me.tbOutput.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(577, 385)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(187, 72)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Vypočítej za mě"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1145, 634)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.tbOutput)
+        Me.Controls.Add(Me.tbInput)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.dgvTable1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MinimumSize = New System.Drawing.Size(1167, 690)
+        Me.Name = "Form1"
+        Me.Text = "Robot BELA"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvTable1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dgvTable1 As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tbInput As TextBox
+    Friend WithEvents tbOutput As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Dim Table1 As DataTable = New DataTable()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Ahoj, já jsem robot Bela v počítači a vypočtám za tebe domácí úkol z matematiky na straně 40")
+        'Create table with n columns
+        Dim tableSize As Int16 = 5
+        Dim startNumber As Int32 = 0
+        Dim stepNumber As Int32 = 1
+        ' Create columns in table
+        For i As Integer = 0 To tableSize - 1
+            Table1.Columns.Add(Chr(65 + i), GetType(System.Int32))
+        Next
+
+        'Populate and add rows to table
+
+
+        'Create table with 5 columns
+        'Table1.Columns.Add("A", GetType(System.Int32))
+        'Table1.Columns.Add("B", GetType(System.Int32))
+        'Table1.Columns.Add("C", GetType(System.Int32))
+        'Table1.Columns.Add("D", GetType(System.Int32))
+        'Table1.Columns.Add("E", GetType(System.Int32))
+        'Add rows
+        Table1.Rows.Add(0, 1, 2, 3, 4)
+        Table1.Rows.Add(5, 6, 7, 8, 9)
+        Table1.Rows.Add(10, 11, 12, 13, 14)
+        Table1.Rows.Add(15, 16, 17, 18, 19)
+        Table1.Rows.Add(20, 21, 22, 23, 24)
+
+        'Show in datagrid
+        dgvTable1.DataSource = Table1
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim count As Int16
+
+        Dim result As List(Of Int32) = New List(Of Integer)
+        If Not Int16.TryParse(tbInput.Text, count) Then
+            MessageBox.Show("Zadej číslo od 1 do 5")
+            Return
+        ElseIf tbInput.Text = 0 Or tbInput.Text > 5 Then
+            MessageBox.Show("Zadej číslo od 1 do 5")
+            Return
+        Else
+            count = tbInput.Text - 1
+        End If
+
+        For k As Integer = 0 To Table1.Rows.Count - 1
+            For i As Integer = 0 To (4 - count)
+                Dim suma As Int32 = 0
+                For j As Integer = i To i + count
+                    suma += Table1.Rows(k).Item(j)
+                Next
+                result.Add(suma)
+            Next
+        Next
+        tbOutput.Text = String.Join(", ", result)
+    End Sub
+End Class
